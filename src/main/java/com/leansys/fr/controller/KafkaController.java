ae632c73028910fc22fba9ef2cf4182dcebe4c6a
am3thyste <ursuludmila2010@gmail.com>
2019-07-13 19:46:56 +0200

    first code commit

---
+++ src/main/java/com/leansys/fr/controller/KafkaController.java
@@ -0,0 +1,25 @@
+package com.leansys.fr.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.leansys.fr.service.KafkaService;
+
+@RestController
+@RequestMapping(value="/kafkamessage")
+public class KafkaController {
+	
+	@Autowired
+	KafkaService kafkaService;
+
+	@GetMapping(value="/producer")
+	public String producer (@RequestParam("message")String message) {
+		kafkaService.send(message);
+		
+		return "OK";
+	}
+	
+}
---
+++ src/main/java/com/leansys/fr/service/KafkaService.java
@@ -0,0 +1,18 @@
+package com.leansys.fr.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaService {
+
+	@Autowired
+	private KafkaTemplate <String, String> kafkaTemplate;
+	
+	String kafkaTopic = "newtopickafka2";
+	
+	public void send(String message) {
+		kafkaTemplate.send(kafkaTopic, message);
+	}
+}
